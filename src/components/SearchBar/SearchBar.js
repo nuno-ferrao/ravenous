7df10d3ba6ae900191bada248dc7559defa41eb1
Nuno Ferrão <nunoferrao@Nunos-MBP.lan>
2020-07-14 17:01:00 +0100

    First part of project

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './SearchBar.css';
+
+
+
+const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+}
+
+
+class SearchBar extends React.Component {
+    // Tudo o que este método faz é iterar pelo objeto sortByOptions e gerar para cada um dos key-value pairs, um elemento <li> com o respetive conteudo e property
+    renderSortByOptions() {
+        return Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption];
+            return <li key={sortByOptionValue}>{sortByOption}</li>;
+        });
+    }
+
+    render() {
+        return (
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                        {this.renderSortByOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a>Let's Go</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default SearchBar;
