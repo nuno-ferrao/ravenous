0f91e4c2c3ad15c72d1935976c21ecfeb212d38e
Nuno Ferrão <nunoferrao@Nunos-MBP.lan>
2020-07-15 11:18:59 +0100

    ravenous part2 update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,13 +4,34 @@ import SearchBar from '../SearchBar/SearchBar';
 import BusinessList from '../BusinessList/BusinessList';
 import { render } from '@testing-library/react';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = []
+
+let i = 0;
+
+while (i < 6) {
+  businesses.push(business);
+  i++;
+}
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses = {businesses}/>
       </div>
     );
   }
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,15 +3,17 @@ import './BusinessList.css';
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
+    // the business array set in app.js is set up as a prop for this component. Here, we are accessing that prop and iterating through the business elements it contains (6 of them) and generating a Business component with a prop that corresponds to each element in the array. That way, we are generating a businesslist with 6 elements each with a prop that will allow us to access all the properties in the business object as set up in App.js
     render() {
         return (
             <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+              {
+                this.props.businesses.map(business => {
+                    return (
+                        <Business business={business} />
+                    )
+                })
+              }
             </div>
         )
     }
